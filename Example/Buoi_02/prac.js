f5b2f1a2a138e2d413c098e6f23f9740971186c8
Phạm Công Vinh <pcv.fed@gmail.com>
2024-11-30 21:51:52 +0700

    react-createElement and DOM createElement

---
+++ Example/Buoi_02/prac.js
@@ -0,0 +1,8 @@
+const divReact = React.createElement(
+  "div",
+  {
+    className: "post-item",
+  },
+  React.createElement("h2DOM", { title: "Học React tại F8" }, "Học ReactJS"),
+  React.createElement("pDOM", null, "Học ReaJS từ cơ bản tới nâng cao")
+);
